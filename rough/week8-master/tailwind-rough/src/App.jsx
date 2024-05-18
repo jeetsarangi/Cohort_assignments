ecd55113957d5275bacb7f0de2affb8b9b6bd84b
Jeet Sarangi <jeetsarangiflash@gmail.com>
2024-05-18 13:12:32 +0530

    first paytm backend done

---
+++ rough/week8-master/tailwind-rough/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='grid grid-cols-1 md:grid-cols-3'>
+      <div className='bg-yellow-500'>hi from the first div</div>
+      <div className='bg-red-950'>hi from the first div</div>
+      <div className='bg-blue-950'>hi from the first div</div>
+    </div>
+
+    </>
+  )
+}
+
+export default App
